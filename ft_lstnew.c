30f0a828e07d29fb53602e3cdbc56ea106351913
tpolonen <merirosvo@kyy.ninja>
2021-11-12 13:51:07 +0200

    more bonus functions

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: tpolonen <tpolonen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/12 10:55:52 by tpolonen          #+#    #+#             */
-/*   Updated: 2021/11/12 11:25:30 by tpolonen         ###   ########.fr       */
+/*   Updated: 2021/11/12 13:42:49 by tpolonen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,19 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 	t_list	*l1;
 	void	*c1;
 
-	l1 = (t_list *) malloc(sizeof(t_list));
-	c1 = malloc(content_size);
-	ft_memcpy(c1, content, content_size);
-	l1->content = c1;
+	l1 = (t_list *) ft_memalloc(sizeof(t_list));
+	if (content == NULL)
+	{
+		l1->content = NULL;
+		l1->content_size = 0;
+	}
+	else
+	{
+		c1 = ft_memalloc(content_size);
+		ft_memcpy(c1, content, content_size);
+		l1->content_size = content_size;
+		l1->content = c1;
+	}
+	l1->next = NULL;
 	return (l1);
 }
