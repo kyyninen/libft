eb30a8ec02f895de8a312a7280480fe845da41b3
tpolonen <merirosvo@kyy.ninja>
2021-11-12 18:10:29 +0200

    not trusting the lstmap function yet

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,10 +6,10 @@
 /*   By: tpolonen <tpolonen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/12 15:43:45 by tpolonen          #+#    #+#             */
-/*   Updated: 2021/11/12 16:09:53 by tpolonen         ###   ########.fr       */
+/*   Updated: 2021/11/12 18:09:02 by tpolonen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-/*
+
 #include "libft.h"
 #include <stdlib.h>
 
@@ -18,9 +18,18 @@ t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 	t_list	*head;
 	t_list	*h1;
 
-	head = ft_lstnew(lst->content, lst->content_size);
-	if (!head)
+	head = f(lst);
+	if (!lst)
 		return (NULL);
+	lst = lst->next;
 	h1 = head;
-	while (lst->next)
-} work in progress*/
+	while (lst != NULL)
+	{
+		h1->next = f(lst);
+		if (!h1->next)
+			return (NULL);
+		lst = lst->next;
+		h1 = h1->next;
+	}
+	return (head);
+}
