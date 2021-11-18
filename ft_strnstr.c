38b66d56911912f449249f65a86073e9280b5e7a
tpolonen <merirosvo@kyy.ninja>
2021-11-18 23:47:53 +0200

    rewrote strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,34 +6,26 @@
 /*   By: tpolonen <tpolonen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 19:26:44 by tpolonen          #+#    #+#             */
-/*   Updated: 2021/11/18 16:31:42 by tpolonen         ###   ########.fr       */
+/*   Updated: 2021/11/18 23:14:39 by tpolonen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include <string.h>
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	char	*h1;
-	char	*n1;
-	char	*stop;
+	size_t		n_len;
 
 	if (*needle == '\0')
 		return ((char *) haystack);
-	stop = (char *) haystack + len;
-	while (*haystack && haystack < stop)
+	n_len = ft_strlen(needle);
+	while (*haystack && len >= n_len)
 	{
-		while (*haystack != *needle && *haystack)
-			haystack++;
-		if (*haystack == *needle)
-		{
-			h1 = (char *) haystack;
-			n1 = (char *) needle;
-			while (*h1++ == *n1++ && h1 <= stop)
-				if (*n1 == '\0')
-					return ((char *) haystack);
-		}
+		if (*haystack == *needle && !ft_strncmp(haystack, needle, n_len))
+			return ((char *) haystack);
 		haystack++;
+		len--;
 	}
 	return (NULL);
 }
