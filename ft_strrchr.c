b2319b7614cd6899154902e495e11328e9ab0b59
tpolonen <tpolonen@student.hive.fi>
2021-11-19 11:13:56 +0200

    final normi fix?

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: tpolonen <tpolonen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 15:45:18 by tpolonen          #+#    #+#             */
-/*   Updated: 2021/11/18 16:33:04 by tpolonen         ###   ########.fr       */
+/*   Updated: 2021/11/19 11:12:37 by tpolonen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char	*ft_strrchr(const char *s, int c)
 {
 	const char	*s1;
 
-	s1 =  s;
+	s1 = s;
 	s1 += (ft_strlen(s));
 	while (s1 >= s)
 	{
