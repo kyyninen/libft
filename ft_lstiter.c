486d2c362f8fd79c20949f3aa97ea9faf0a5ad55
tpolonen <merirosvo@kyy.ninja>
2021-11-21 17:21:00 +0200

    how these are not fixed at this point

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,13 +6,13 @@
 /*   By: tpolonen <tpolonen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/12 14:47:23 by tpolonen          #+#    #+#             */
-/*   Updated: 2021/11/12 19:16:54 by tpolonen         ###   ########.fr       */
+/*   Updated: 2021/11/21 17:14:51 by tpolonen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstiter(t_list *lst, void(*f)(t_list *elem))
+void	ft_lstiter(t_list *lst, void (*f)(t_list *elem))
 {
 	f(lst);
 	if (lst->next != NULL)
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tpolonen <tpolonen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 17:26:45 by tpolonen          #+#    #+#             */
-/*   Updated: 2021/11/21 16:34:31 by tpolonen         ###   ########.fr       */
+/*   Updated: 2021/11/21 17:15:20 by tpolonen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,6 +85,6 @@ void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
 void	ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void	ft_lstadd(t_list **alst, t_list *new);
 void	ft_lstadd_back(t_list **alst, t_list *new);
-void	ft_lstiter(t_list *lst, void(*f)(t_list *elem));
+void	ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 #endif
